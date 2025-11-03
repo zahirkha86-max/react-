9f4c55ab707c3709f62040f9efa312fa8b6225b2
zahir <zahirkha86@gmail.com>
2025-11-03 19:21:39 +0530

    last change

--- src/Hcard.jsx
+++ src/Hcard.jsx
@@ -1,49 +1,49 @@
 import Hcard from './Hcardcss.module.css'
 
 let Hcards=()=>{
-    // let data=[
-    //     {
-    //         Imageurl:"./image.png",
-    //         Description:"i am fellow developer",
-    //     },
-    //             {
-    //         Imageurl:"./image1.png",
-    //         Description:"i am fellow app developer",
-    //     },
-    //       {
-    //         Imageurl:"./image.png",
-    //         Description:"i am fellow web developer",
-    //     }
+    let data=[
+        {
+            Imageurl:"./image.png",
+            Description:"i am fellow developer",
+        },
+                {
+            Imageurl:"./image1.png",
+            Description:"i am fellow app developer",
+        },
+          {
+            Imageurl:"./image.png",
+            Description:"i am fellow web developer",
+        }
 
-    // ];
+    ];
 
-let data=[
-{
-    Imgurl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRk2aITKEes3IYig_VkU7s5sfK6hwuSroaTfA&s",
+// let data=[
+// {
+//     Imgurl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRk2aITKEes3IYig_VkU7s5sfK6hwuSroaTfA&s",
 
-    Discription:"Holiday-Themed Resources"
-},
+//     Discription:"Holiday-Themed Resources"
+// },
 
-{
-    Imgurl:"https://daily.jstor.org/wp-content/uploads/2015/07/school_1050x700.jpg",
-    Discription:"Project-Based Learing"
-},
-{
-    Imgurl:"https://www.thegaudium.com/wp-content/uploads/2016/08/Pooja0002.jpg",
-    Discription:"Professional Devlopment"
-},
+// {
+//     Imgurl:"https://daily.jstor.org/wp-content/uploads/2015/07/school_1050x700.jpg",
+//     Discription:"Project-Based Learing"
+// },
+// {
+//     Imgurl:"https://www.thegaudium.com/wp-content/uploads/2016/08/Pooja0002.jpg",
+//     Discription:"Professional Devlopment"
+// },
 
-{
-    Imgurl:"https://www.unesco.org/sites/default/files/styles/paragraph_medium_desktop/article/2023-09/school%20girl%20thailand.jpg?itok=QSNKTR12",
-    Discription:"Straegies For the Classroom"
-}
+// {
+//     Imgurl:"https://www.unesco.org/sites/default/files/styles/paragraph_medium_desktop/article/2023-09/school%20girl%20thailand.jpg?itok=QSNKTR12",
+//     Discription:"Straegies For the Classroom"
+// }
 
-]
+// ]
 
 
     return(
         <>
-        {/* <div className={Hcard.grid}>
+        { <div className={Hcard.grid}>
             {data.map((e,i)=>{
                 return <div key={i}>
                     <img src={e.Imageurl} alt="" />
@@ -52,7 +52,7 @@ let data=[
                 </div>
             })}
       
-        </div> */}
+        </div> }
 
         {
             <div className={Hcard.grid}>
